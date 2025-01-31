c0dbb25f9206994f85f87cd5c9ef5dd79fef29bf
BleronaHaxhiu <bleronahaxhiu5@gmail.com>
2025-01-31 17:34:12 +0100

    Config and connecting with db

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+import express from 'express'
+const app = express()
+import dotenv from 'dotenv';
+import { connectDataBase } from './config/dbConnect.js';
+
+
+dotenv.config({ path:"backend/config/config.env"});
+
+connectDataBase();
+
+
+import productRoutes from "./routes/products.js";
+
+
+app.use("/api/v1",productRoutes);
+
+app.listen (process.env.PORT ,() => {
+    console.log(`Server started on port : ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
+});
---
+++ backend/models/product.js
@@ -0,0 +1,99 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"Please enter product name"],
+        maxLength:[200,"Product name cannot exceed 200 characters"],
+    },
+    price:{
+        type:Number,
+        required:[true,"Please enter product price"],
+        maxLength:[5,"Product price cannot exceed 5 digits"],
+    },
+    description:{
+        type:String,
+        required:[true,"Please enter product description"],
+    },
+    ratings:{
+        type:Number,
+        default:0
+    },
+    images:[
+        {
+        public_id:
+        {
+            type:String,
+            required:True,
+        },
+        url:{
+            type:String,
+            required:True,
+        },
+    },
+    ],
+    category:{
+
+        type:String,
+        required:[true,"Please enter product category"],
+        enum:{
+            values:[
+                "Laptops",
+                "Electronics",
+                "Cameras",
+                "Accesories",
+                "Headphones",   
+                "Food",
+                "Books",
+                "Sports",
+                "Outdoor",
+                "Home",
+            ],
+            message:"Please select correct category",
+
+        },
+    },
+    seller:{
+        type:String,
+        required:[true,"Please enter product seller"],
+    },
+    stock:{
+        type:String,
+        required:[true,"Please enter product stock"],
+    },
+    numOfReviews:{
+        type:Number,
+        default:0,
+    },
+    reviews:[
+        {
+            user:{
+                type: mongoose.Schema.Types.ObjectId,
+                ref:'User',
+                required:true,
+        },
+        rating:{
+            type:Number,
+            required:True,
+        },
+        comment:{
+            type:String,
+            required:True,
+        },
+        },
+    ],
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true,
+},
+createdAt:{
+    type:Date,
+    default:Date.nov,
+},
+
+    
+},
+ {timestamps :true},
+);
+export default mongoose.model("Product",productSchema);
