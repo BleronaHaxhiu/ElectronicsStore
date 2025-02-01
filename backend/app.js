55ce6d0c089cf28074fa6cc71084d33e6ccb54db
HavaAhmeti26 <havaahmeti2003@gmail.com>
2025-02-01 19:30:28 +0100

    "Ndryshime tjera"

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,16 @@ import express from 'express'
 const app = express()
 import dotenv from 'dotenv';
 import { connectDataBase } from './config/dbConnect.js';
+import errorMiddleware from './middlewares/errors.js';
+
+
+//handle uncaught exceptions
+
+process.on("uncaughtException", (err) => {
+    console.log(`ERROR: ${err}`);
+    console.log("Shutting down server due to uncaught exception");
+     process.exit(1);
+});
 
 
 dotenv.config({ path:"backend/config/config.env"});
@@ -16,6 +26,20 @@ import productRoutes from "./routes/products.js";
 
 app.use("/api/v1",productRoutes);
 
-app.listen (process.env.PORT ,() => {
+//using error middleware
+app.use(errorMiddleware);
+
+const server = app.listen (process.env.PORT ,() => {
     console.log(`Server started on port : ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
+});
+
+//handle unhandled promise rejections
+
+process.on("unhandledRejection", (err) => {
+    console.log(`ERROR: ${err}`);
+    console.log("Shutting down server due to unhandled promise rejection");
+    server.close(() =>  {
+        process.exit(1);
+    });
+
 });
