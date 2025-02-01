bd22ac124cfc1758e72b6fc7cc9ba809a394ca18
HavaAhmeti26 <havaahmeti2003@gmail.com>
2025-02-01 19:26:42 +0100

    "Seeder"

---
+++ backend/seeder/seeder.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import products from "./data.js";
+import Product from "../models/product.js";
+
+const seedProducts = async () => {
+    try{
+    await mongoose.connect("mongodb+srv://bleronahaxhiu5:lab1234@cluster0.tgken.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+
+    await Product.deleteMany();
+    console.log("Products are deleted");
+
+    await Product.insertMany(products);
+    console.log("Products are added");
+    
+    process.exit();
+
+    }catch(error){
+        console.log(error.message);
+        process.exit();
+    }
+
+};
+seedProducts();
